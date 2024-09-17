f0410a82c2b0b570d212eff7d0760bdbc0b96cac
JavierUgarrio <j.ugarrio.duro@gmail.com>
2024-09-17 11:15:21 +0200

    Ejercicio Java palindromo

--- Palindromos/src/palindromos/Palindromos.java
+++ Palindromos/src/palindromos/Palindromos.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package palindromos;
+
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +12,24 @@ public class Palindromos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       Scanner teclado = new Scanner(System.in);
+       System.out.println("Por favor, introduce una palabra");
+       String cadena = teclado.nextLine();
+       if(esPalindromo(cadena)){
+           System.out.println("Es un palindromo");
+       }else{
+           System.out.println("No es un palindromo");
+       }
     }
     
+    static boolean esPalindromo(String cadena){
+        cadena =cadena.replace(" ","");
+        cadena =cadena.toLowerCase();
+        StringBuilder cadenaInvertida = new StringBuilder();
+        
+        for(int i= cadena.length()-1; i>=0; i--){
+            cadenaInvertida.append(cadena.charAt(i));
+        }
+        return cadena.equals(cadenaInvertida.toString());
+    }
 }
